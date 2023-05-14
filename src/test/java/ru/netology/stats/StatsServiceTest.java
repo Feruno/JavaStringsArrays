89a60acfb3a6f767e6beeccdffd8e5f5d45b1191
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-05-14 15:06:28 +0300

    rework home work

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -66,7 +66,7 @@ public class StatsServiceTest {
         int[] salesByMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedMaxAverage = 5;
 
-        int actualMaxAverage = service.monthsSalesBelowAverage(salesByMonth);
+        int actualMaxAverage = service.monthsSalesMaxAverage(salesByMonth);
         System.out.println(actualMaxAverage);
         Assertions.assertEquals(expectedMaxAverage, actualMaxAverage);
     }
